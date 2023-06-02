fc9f72f91bc07904b354314b6fb06ffee682af6f
HuuTam <christopherle0902@gmail.com>
2023-06-02 17:30:10 +0700

    update list user

--- src/components/TableUsers.js
+++ src/components/TableUsers.js
@@ -1,7 +1,44 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { fetchAllUser } from "../services/UserService";
+import Table from "react-bootstrap/Table";
 const TableUsers = () => {
-  return <div>TableUsers</div>;
+  const [listUser, setListUser] = useState([]);
+  useEffect(() => {
+    getUsers();
+  }, []);
+  console.log("check list user", listUser);
+  const getUsers = async () => {
+    let res = await fetchAllUser();
+    if (res && res.data && res.data.data) {
+      setListUser(res.data.data);
+    }
+  };
+  return (
+    <>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Email</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listUser &&
+            listUser.length > 0 &&
+            listUser.map((item, index) => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.email}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </>
+  );
 };
 
 export default TableUsers;
