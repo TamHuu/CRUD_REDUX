d53029f8516bfc78f16963dc25082b1011bd1c9e
HuuTam <christopherle0902@gmail.com>
2023-06-30 14:33:09 +0700

    add

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "react-toastify/dist/ReactToastify.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
+
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./context/UserContext";
 import { Provider } from "react-redux";
